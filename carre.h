6dc1522ee1e5dec387c4ad56c76c6b2d6db12312
tom limouzin <tomlimouzin2005@gmail.com>
2024-12-03 09:10:32 +0100

    Commit#3

--- carre.h
+++ carre.h
@@ -1,2 +1,33 @@
 #pragma once
-void CCarre(float sx, float sy, unsigned int* cote);
+#include <iostream>
+
+class CCarre {
+private:
+    int sx;   // Coordonnée X du carré
+    int sy;   // Coordonnée Y du carré
+    int cote; // Longueur du côté du carré
+
+public:
+    /**
+     * Affecte une valeur à la coordonnée X (sx).
+     * sx1 Valeur entière à affecter à sx.
+     */
+    void Setsx(int sx1);
+
+    /**
+     *Affecte une valeur à la coordonnée Y (sy).
+     *sy1 Valeur entière à affecter à sy.
+     */
+    void Setsy(int sy1);
+
+    /**
+     *Affecte une valeur à la longueur du côté du carré (cote).
+     * cote1 Valeur entière à affecter à cote.
+     */
+    void Setcote(int cote1);
+
+    /**
+     *  Affiche les caractéristiques du carré (sx, sy, cote).
+     */
+    void Afficher();
+};
--- mcarre.cpp
+++ mcarre.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "carre.h"
 
 
 using namespace std;
